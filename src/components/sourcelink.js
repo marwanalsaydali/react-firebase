4be4f597cdbe58deb2b8a29cf7d5bf141a5b4c80
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-02-09 21:06:01 -0500

    source link components to be reuse

---
+++ src/components/sourcelink.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
+
+const SourceLink = () => {
+  return (
+    <div className="source-link">
+      <AiFillGithub />
+    </div>
+  );
+};
+
+export default SourceLink;
