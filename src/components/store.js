8d11a3dad2ac5929937edbdfe4bd83674f04e1cb
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-02-10 19:55:59 -0500

    component that we will use later

---
+++ src/components/store.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Store extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="store">Store</div>
+      </>
+    );
+  }
+}
+
+export default Store;
