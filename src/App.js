856e962b4c0e6e9f5c763b24190b6d4a904ba09f
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-02-10 19:55:32 -0500

    Buildong the state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import './App.css'
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+import React from "react";
+import Layout from "./layout/layout";
+
+import Store from "./components/store";
+import Order from "./components/order";
+import Inventory from "./components/inventory";
+
+import "./App.css";
+class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+  addFish = (fish) => {
+    // first copy what in the state
+    const fishes = { ...this.state.fishes };
+    //// add the new fish to that copy
+    fishes[`fish${Date.now()}`] = fish;
+    // set the new state
+    this.setState({ fishes: fishes });
+  };
+  render() {
+    return (
+      <Layout>
+        <div className="dashboard">
+          <Store />
+          <Order />
+          <Inventory addFish={this.addFish} />
+        </div>
+      </Layout>
+    );
+  }
 }
 
 export default App;
