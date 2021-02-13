15d39f55e5087588382e9a9bf725e22d8676a0cf
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-02-12 21:09:54 -0500

    The fish info component

---
+++ src/components/fish.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Fish = (props) => {
+  const { name, image, desc, price, status } = props.details;
+  const isAvailable = status === "available";
+  const addToOrder = () => {
+    props.addToOrder(props.index);
+  };
+  return (
+    <li>
+      <img src={image} />
+      <h3>{name}</h3>
+      <span>{price}</span>
+      <p>{desc}</p>
+      <button disabled={!isAvailable} onClick={addToOrder}>
+        {isAvailable ? "Add to Order" : "Sold Out"}
+      </button>
+    </li>
+  );
+};
+
+export default Fish;
