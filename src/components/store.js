12ac61f7b195b1580a6014656c7b1ebcf2da8c5e
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-02-12 21:10:38 -0500

    sort the list of the fishes

--- src/components/store.js
+++ src/components/store.js
@@ -1,10 +1,19 @@
 import React from "react";
-
+import Fish from "./fish";
 class Store extends React.Component {
   render() {
     return (
       <>
-        <div className="store">Store</div>
+        <div className="store">
+          {Object.keys(this.props.fishes).map((key) => (
+            <Fish
+              index={key}
+              key={key}
+              details={this.props.fishes[key]}
+              addToOrder={this.props.addToOrder}
+            />
+          ))}
+        </div>
       </>
     );
   }
