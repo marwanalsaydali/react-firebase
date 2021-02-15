057f4f61068a25edf05dcc8c4c04f3c58c792a27
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-02-14 23:00:08 -0500

    the 404

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../layout/layout";
+const NotFound = () => {
+  return (
+    <Layout>
+      <h1>Not Found</h1>
+    </Layout>
+  );
+};
+
+export default NotFound;
