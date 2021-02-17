b3cf63a0f1f9dc7eba102a9bad1db5fdae415715
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-02-16 20:54:01 -0500

    Social media links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer>
+      <>
+        <AiFillGithub />
+        <AiFillLinkedin />
+        <AiOutlineMail />
+        <AiOutlineInstagram />
+        <AiOutlineTwitter />
+      </>
+    </footer>
+  );
 };
 
 export default Footer;
