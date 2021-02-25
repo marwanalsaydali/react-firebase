3c8b32f2c510c1b59f11156a3ed295f73ebb4bb6
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-02-24 19:42:42 -0500

    styling the nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,9 +2,11 @@ import React from "react";
 import SourceLink from "./sourcelink";
 const Nav = () => {
   return (
-    <nav>
-      <p>Nav</p>
-      <SourceLink />
+    <nav className="sticky top-0 bg-black h-12 shadow-lg flex items-center">
+      <div className="container mx-auto flex flex-row justify-between items-center w-full">
+        <p className="text-gray-100">Nav</p>
+        <SourceLink />
+      </div>
     </nav>
   );
 };
--- src/components/sourcelink.js
+++ src/components/sourcelink.js
@@ -3,7 +3,7 @@ import { AiFillGithub } from "react-icons/ai";
 
 const SourceLink = () => {
   return (
-    <div className="source-link">
+    <div className="source-link fill-current text-gray-100">
       <AiFillGithub />
     </div>
   );
